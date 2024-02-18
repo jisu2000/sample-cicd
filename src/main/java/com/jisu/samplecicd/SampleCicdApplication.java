80194d446930b11a214aab3b01e0a4627a46ee49
jisu2000 <spjisu2000@gmail.com>
2024-02-18 22:42:04 +0530

    first commmit

---
+++ src/main/java/com/jisu/samplecicd/SampleCicdApplication.java
@@ -0,0 +1,13 @@
+package com.jisu.samplecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jisu/samplecicd/SampleCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisu.samplecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
